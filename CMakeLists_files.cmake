657935d0712e8416a2f053c2ded363cd71b9db69
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2022-10-19 09:55:14 +0200

    added: WellConstraints
    this will hold the code for well constraints.
    start by moving activeProductionControl to the new class

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -101,6 +101,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/wells/VFPProdProperties.cpp
   opm/simulators/wells/VFPInjProperties.cpp
   opm/simulators/wells/WellBhpThpCalculator.cpp
+  opm/simulators/wells/WellConstraints.cpp
   opm/simulators/wells/WellGroupConstraints.cpp
   opm/simulators/wells/WellGroupControls.cpp
   opm/simulators/wells/WellGroupHelpers.cpp
@@ -379,6 +380,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/wells/VFPProperties.hpp
   opm/simulators/wells/WellConnectionAuxiliaryModule.hpp
   opm/simulators/wells/WellBhpThpCalculator.hpp
+  opm/simulators/wells/WellConstraints.hpp
   opm/simulators/wells/WellGroupConstraints.hpp
   opm/simulators/wells/WellGroupControls.hpp
   opm/simulators/wells/WellGroupHelpers.hpp
