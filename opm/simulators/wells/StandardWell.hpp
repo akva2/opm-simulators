cf1d3c65a0ecf7fea5bb624e2dfd9eeae9718f2a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-05-05 09:55:26 +0200

    changed: put calculation of gas-water perf rates in separate method

--- opm/simulators/wells/StandardWell.hpp
+++ opm/simulators/wells/StandardWell.hpp
@@ -479,6 +479,12 @@ namespace Opm
                                    const Value& rv,
                                    const Value& rs,
                                    const Value& rvw) const;
+
+        template<class Value>
+        void computeGasWaterPerfRate(std::vector<Value>& cq_s,
+                                     PerforationRates& perf_rates,
+                                     const Value& rvw,
+                                     const Value& rsw) const;
     };
 
 }
