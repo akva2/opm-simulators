8344b1bcf03d7c1676338f2a41c7b1f8164e4063
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-05-07 08:42:44 +0200

    changed: move computeGasOilPerfRate to WellPerforations.cpp

--- opm/simulators/wells/StandardWell.hpp
+++ opm/simulators/wells/StandardWell.hpp
@@ -474,13 +474,6 @@ namespace Opm
 
     private:
         template<class Value>
-        void computeGasOilPerfRate(std::vector<Value>& cq_s,
-                                   PerforationRates& perf_rates,
-                                   const Value& rv,
-                                   const Value& rs,
-                                   const Value& rvw) const;
-
-        template<class Value>
         void computeGasOilPerfRateCrossflow(const std::vector<Value>& cq_s,
                                             PerforationRates& perf_rates,
                                             const Value& rv,
