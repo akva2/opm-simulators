d6fe2155f4f504be865d16df36a61992775641cd
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-01 11:14:15 +0100

    FlowSolvent: drop Ebos in function names

--- opm/simulators/flow/Main.hpp
+++ opm/simulators/flow/Main.hpp
@@ -639,7 +639,7 @@ private:
 
         // solvent + gas + water + oil
         if (phases.active( Phase::OIL ) && phases.active( Phase::WATER ) && phases.active( Phase::GAS )) {
-            return flowEbosSolventMain(argc_, argv_, outputCout_, outputFiles_);
+            return flowSolventMain(argc_, argv_, outputCout_, outputFiles_);
         }
 
         if (outputCout_)
