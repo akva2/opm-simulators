35f3f6fa6e1c1ba8513a5e7487df6075ed6b1617
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-02 10:46:44 +0100

    move ebos/alucartesianindexmapper.hh to opm/simulators/flow

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -402,7 +402,6 @@ list (APPEND TEST_DATA_FILES
 # originally generated with the command:
 # find opm -name '*.h*' -a ! -name '*-pch.hpp' -printf '\t%p\n' | sort
 list (APPEND PUBLIC_HEADER_FILES
-  ebos/alucartesianindexmapper.hh
   ebos/collecttoiorank.hh
   ebos/collecttoiorank_impl.hh
   ebos/eclalugridvanguard.hh
@@ -439,6 +438,7 @@ list (APPEND PUBLIC_HEADER_FILES
   ebos/femcpgridcompat.hh
   ebos/vtkecltracermodule.hh
   opm/simulators/flow/ActionHandler.hpp
+  opm/simulators/flow/AluGridCartesianIndexMapper.hpp
   opm/simulators/flow/BlackoilModel.hpp
   opm/simulators/flow/BlackoilModelNldd.hpp
   opm/simulators/flow/BlackoilModelParameters.hpp
