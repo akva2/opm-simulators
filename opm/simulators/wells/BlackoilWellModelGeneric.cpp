a43f83121dc4b5614682c8bdeba5892087d3ba43
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-19 15:46:29 +0100

    GasLiftGroupInfo: template Scalar type

--- opm/simulators/wells/BlackoilWellModelGeneric.cpp
+++ opm/simulators/wells/BlackoilWellModelGeneric.cpp
@@ -1384,7 +1384,7 @@ void BlackoilWellModelGeneric<Scalar>::
 gasLiftOptimizationStage2(DeferredLogger& deferred_logger,
                           GLiftProdWells& prod_wells,
                           GLiftOptWells& glift_wells,
-                          GasLiftGroupInfo& group_info,
+                          GasLiftGroupInfo<Scalar>& group_info,
                           GLiftWellStateMap& glift_well_state_map,
                           const int episodeIndex)
 {
