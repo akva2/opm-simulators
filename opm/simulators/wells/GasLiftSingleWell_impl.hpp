a77d6aa30483b26af981c57b4fe30fc537b7ed29
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-20 09:05:09 +0100

    GasLiftSingleWell: use Scalar type

--- opm/simulators/wells/GasLiftSingleWell_impl.hpp
+++ opm/simulators/wells/GasLiftSingleWell_impl.hpp
@@ -20,37 +20,37 @@
 #include <opm/input/eclipse/Schedule/GasLiftOpt.hpp>
 #include <fmt/format.h>
 
+#include <string>
+#include <vector>
+
 namespace Opm {
 
 template<typename TypeTag>
 GasLiftSingleWell<TypeTag>::
-GasLiftSingleWell(const WellInterface<TypeTag> &well,
-                  const Simulator &ebos_simulator,
-                  const SummaryState &summary_state,
-                  DeferredLogger &deferred_logger,
+GasLiftSingleWell(const WellInterface<TypeTag>& well,
+                  const Simulator& ebos_simulator,
+                  const SummaryState& summary_state,
+                  DeferredLogger& deferred_logger,
                   WellState<Scalar>& well_state,
                   const GroupState<Scalar>& group_state,
                   GasLiftGroupInfo<Scalar>& group_info,
                   GLiftSyncGroups &sync_groups,
                   const Parallel::Communication& comm,
-                  bool glift_debug
-                 )
+                  bool glift_debug)
     // The parent class GasLiftSingleWellGeneric contains all stuff
     //   that is not dependent on TypeTag
-    : GasLiftSingleWellGeneric<Scalar>(
-        deferred_logger,
-        well_state,
-        group_state,
-        well.wellEcl(),
-        summary_state,
-        group_info,
-        well.phaseUsage(),
-        ebos_simulator.vanguard().schedule(),
-        ebos_simulator.episodeIndex(),
-        sync_groups,
-        comm,
-        glift_debug
-    )
+    : GasLiftSingleWellGeneric<Scalar>(deferred_logger,
+                                       well_state,
+                                       group_state,
+                                       well.wellEcl(),
+                                       summary_state,
+                                       group_info,
+                                       well.phaseUsage(),
+                                       ebos_simulator.vanguard().schedule(),
+                                       ebos_simulator.episodeIndex(),
+                                       sync_groups,
+                                       comm,
+                                       glift_debug)
    , ebos_simulator_{ebos_simulator}
    , well_{well}
 {
@@ -70,7 +70,7 @@ GasLiftSingleWell(const WellInterface<TypeTag> &well,
     //   If gas lift optimization has not been applied to this well yet, the
     //   default value is used.
     this->orig_alq_ = this->well_state_.getALQ(this->well_name_);
-    if(this->optimize_) {
+    if (this->optimize_) {
         this->setAlqMinRate_(gl_well);
         // NOTE: According to item 4 in WLIFTOPT, this value does not
         //    have to be positive.
@@ -102,11 +102,13 @@ GasLiftSingleWell(const WellInterface<TypeTag> &well,
 template<typename TypeTag>
 typename GasLiftSingleWell<TypeTag>::BasicRates
 GasLiftSingleWell<TypeTag>::
-computeWellRates_( double bhp, bool bhp_is_limited, bool debug_output ) const
+computeWellRates_(Scalar bhp, bool bhp_is_limited, bool debug_output ) const
 {
-    std::vector<double> potentials(this->NUM_PHASES, 0.0);
-    this->well_.computeWellRatesWithBhp(
-        this->ebos_simulator_, bhp, potentials, this->deferred_logger_);
+    std::vector<Scalar> potentials(this->NUM_PHASES, 0.0);
+    this->well_.computeWellRatesWithBhp(this->ebos_simulator_,
+                                        bhp,
+                                        potentials,
+                                        this->deferred_logger_);
     if (debug_output) {
         const std::string msg = fmt::format("computed well potentials given bhp {}, "
             "oil: {}, gas: {}, water: {}", bhp,
@@ -126,9 +128,9 @@ computeWellRates_( double bhp, bool bhp_is_limited, bool debug_output ) const
 }
 
 template<typename TypeTag>
-std::optional<double>
+std::optional<typename GasLiftSingleWell<TypeTag>::Scalar>
 GasLiftSingleWell<TypeTag>::
-computeBhpAtThpLimit_(double alq, bool debug_output) const
+computeBhpAtThpLimit_(Scalar alq, bool debug_output) const
 {
     auto bhp_at_thp_limit = this->well_.computeBhpAtThpLimitProdWithAlq(
         this->ebos_simulator_,
@@ -199,7 +201,7 @@ setupPhaseVariables_()
 template<typename TypeTag>
 void
 GasLiftSingleWell<TypeTag>::
-setAlqMaxRate_(const GasLiftWell &well)
+setAlqMaxRate_(const GasLiftWell& well)
 {
     auto& max_alq_optional = well.max_rate();
     if (max_alq_optional) {
