78736835bbee6a45ad0792c84abcb0ed0ee111b1
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-02-04 17:36:53 +0100

    move allocation of mech data into MechContainer

--- opm/simulators/flow/MechContainer.cpp
+++ opm/simulators/flow/MechContainer.cpp
@@ -25,5 +25,68 @@
 
 namespace Opm {
 
+template<class Scalar>
+void MechContainer<Scalar>::
+allocate(const std::size_t bufferSize,
+         std::map<std::string, int>& rstKeywords)
+{
+    this->potentialForce_.resize(bufferSize, 0.0);
+    rstKeywords["MECHPOTF"] = 0;
+    this->potentialTempForce_.resize(bufferSize, 0.0);
+    rstKeywords["TEMPPOTF"] = 0;
+    this->potentialPressForce_.resize(bufferSize, 0.0);
+    rstKeywords["PRESPOTF"] = 0;
+
+    this->dispX_.resize(bufferSize, 0.0);
+    rstKeywords["DISPX"] = 0;
+    this->dispY_.resize(bufferSize, 0.0);
+    rstKeywords["DISPY"] = 0;
+    this->dispZ_.resize(bufferSize, 0.0);
+    rstKeywords["DISPZ"] = 0;
+    this->stressXX_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSXX"] = 0;
+    this->stressYY_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSYY"] = 0;
+    this->stressZZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSZZ"] = 0;
+    this->stressXY_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSXY"] = 0;
+    this->stressXZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSXZ"] = 0;
+    this->stressYZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRESSYZ"] = 0;
+
+    this->strainXX_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINXX"] = 0;
+    this->strainYY_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINYY"] = 0;
+    this->strainZZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINZZ"] = 0;
+    this->strainXY_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINXY"] = 0;
+    this->strainXZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINXZ"] = 0;
+    this->strainYZ_.resize(bufferSize, 0.0);
+    rstKeywords["STRAINYZ"] = 0;
+
+    this->delstressXX_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRXX"] = 0;
+    this->delstressYY_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRYY"] = 0;
+    this->delstressZZ_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRZZ"] = 0;
+    this->delstressXY_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRXY"] = 0;
+    this->delstressXZ_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRXZ"] = 0;
+    this->delstressYZ_.resize(bufferSize, 0.0);
+    rstKeywords["DELSTRYZ"] = 0;
+}
+
+template class MechContainer<double>;
+
+#if FLOW_INSTANTIATE_FLOAT
+template class MechContainer<float>;
+#endif
 
 } // namespace Opm
