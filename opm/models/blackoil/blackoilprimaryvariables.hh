b27f1d13ba0410f9a830b503590acf78c6bc5418
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-05-12 13:32:46 +0200

    blackoilprimaryvariables: use std::size_t

--- opm/models/blackoil/blackoilprimaryvariables.hh
+++ opm/models/blackoil/blackoilprimaryvariables.hh
@@ -46,6 +46,7 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
 #include <stdexcept>
 #include <type_traits>
@@ -182,7 +183,7 @@ public:
         result.primaryVarsMeaningPressure_ = PressureMeaning::Pg;
         result.primaryVarsMeaningWater_ = WaterMeaning::Rsw;
         result.primaryVarsMeaningSolvent_ = SolventMeaning::Ss;
-        for (size_t i = 0; i < result.size(); ++i) {
+        for (std::size_t i = 0; i < result.size(); ++i) {
             result[i] = i+1;
         }
 
