857332a6ceb78517373332ce121c7a2fcf23e5f5
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-05-14 13:10:19 +0200

    baseoutputwriter: remove parameter default

--- opm/models/discretization/common/fvbaseproblem.hh
+++ opm/models/discretization/common/fvbaseproblem.hh
@@ -790,7 +790,7 @@ public:
         defaultVtkWriter_->beginWrite(t);
         model().prepareOutputFields();
         model().appendOutputFields(*defaultVtkWriter_);
-        defaultVtkWriter_->endWrite();
+        defaultVtkWriter_->endWrite(false);
     }
 
     /*!
