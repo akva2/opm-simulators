2d19bf4d72a12559da5178bcaeef1fae8e35dc79
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-06-05 14:10:48 +0200

    polymerConcentrationIdx: make unsigned

--- opm/models/blackoil/blackoiltwophaseindices.hh
+++ opm/models/blackoil/blackoiltwophaseindices.hh
@@ -148,8 +148,8 @@ struct BlackOilTwoPhaseIndices
         enableExtbo ? PVOffset + numPhases + numSolvents : std::numeric_limits<unsigned>::max();
 
     //! Index of the primary variable for the first polymer
-    static constexpr int polymerConcentrationIdx =
-        enablePolymer ? PVOffset + numPhases + numSolvents : -1000;
+    static constexpr unsigned polymerConcentrationIdx =
+        enablePolymer ? PVOffset + numPhases + numSolvents : std::numeric_limits<unsigned>::max();
 
     //! Index of the primary variable for the second polymer primary variable (molecular weight)
     static constexpr int polymerMoleWeightIdx =
